5fe839df7fbedf22d9ec8db60591bd9aa6fc0023
JaiSanjay74 <perunkarunai@gmail.com>
2025-04-11 20:51:06 +0530

    Accidently occured Authentication error solved

--- Galaxy_Server/server.js
+++ Galaxy_Server/server.js
@@ -27,20 +27,28 @@ server.use(async(rq,rs,nxt)=>{
 server.use(async(r,rs,nxt)=>{
 
   // for checking that are this incoming request non authenticatable request
-  let Is_non_authenticatable_request = ["signin","signup","forgetpassword","verifyotp",
-    "changepassword","defualt.png","security",".txt",".js",".ttf",".css",".apk","p"].every((v,k)=>!r.url.includes(v))
-  console.log(r.url)
-  if(!Is_non_authenticatable_request || r.url.length == 1){
-    
+  let Is_non_authenticatable_request
+  for(let req of ["signin","signup","forgetpassword","verifyotp",
+    "changepassword","defualt.png","security","assets",".apk"]){
+        if(r.url.includes(req)){
+          Is_non_authenticatable_request = r.url.includes(req)
+          break
+        }
+  }
+  if(Is_non_authenticatable_request || r.url.length == 1){
+    console.log("this worked !")
     nxt()
   }
   else{
+    console.log(r.headers)
     if(Object.keys(r.headers).includes("authorization")){
       try{
           let jwttoken = r.headers.authorization.split(" ")[1]
           let credentials = jwt.verify(jwttoken,process.env.JWT_SECRET)
+          
           let isLoggedout = (await rs.signoutedJWTS.findOne({JWTS:{$in:[jwttoken]}})) != null
           if(!isLoggedout){
+            
             rs.credFromJWT = credentials
             nxt()
           }
@@ -71,14 +79,20 @@ server.use(async(r,rs,nxt)=>{
 
 server.use(async(rq,rs,next)=>{
   // Body text must be json format : {"RsaEncryptedAeskey":"ttashccucudvfu..somethin","AesEncryptedData":"ddfccddfzdkio..something"}
-  let Is_non_securable_requests = ["security","deleteaccount","postanalytics","homeposts"].every((v,k)=>rq.url.includes(v))
+  let Is_non_securable_requests 
+  for(let comp of ["security","deleteaccount","postanalytics","homeposts"]){
+     if(rq.url.includes(comp)){
+       Is_non_securable_requests = true
+       break
+     }
+  }
 
   if(Is_non_securable_requests || rq.method === "GET"){
     next()
   }
   else{
     let securityReq 
-   
+    
     let DecryptedJSON = await SecurityV1.DecryptAESEncryptedData(rq.body)
     securityReq = JSON.parse(DecryptedJSON)
     securityReq.pubkey = rq.body.pubkey
@@ -128,7 +142,7 @@ server.get("/api/lp/p/p",(r,rs)=>{
 // for security 
 server.get("/security/RsaPublicKey",async(rq,rs)=>{
   let RSAprivatekey = await fs.promises.readFile("./Security/publickey.rsa",{encoding:"utf8"})
-  rs.text(RSAprivatekey)
+  rs.send(RSAprivatekey)
 })
 
 server.listen(process.env.PORT)
